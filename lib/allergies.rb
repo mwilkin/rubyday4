0cdd93d4f5a3d4237fdb8232055588c466428922
Cory Olson and Matt Wilkin <cory+matt@>
2016-06-30 15:01:02 -0700

    refactor allergies

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,11 +1,8 @@
 class Fixnum
   define_method(:allergies) do
-    i = 0
-    allergen = ["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats" ]
     result = []
-    self.to_s(2).reverse.split("").each() do |str|
-      result << allergen[i] if str.to_i == 1
-      i += 1
+    self.to_s(2).reverse.split("").each_with_index do |str, i|
+      result << %w[eggs peanuts shellfish strawberries tomatoes chocolate pollen cats][i] if str.to_i == 1
     end
     result
   end
